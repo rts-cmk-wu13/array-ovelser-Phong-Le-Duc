10f2f5080c4e2308d988ed46967b49517b62f48e
Phong <phongleduc@gmail.com>
2025-02-03 09:04:15 +0100

    opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
+
+    let newNumbers = numbersArray.filter(function (number) {
+        return number > 20
+    });
+    console.log(newNumbers);
+
 
 }) // ends DOMContentLoaded
